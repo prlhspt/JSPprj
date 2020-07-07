cb3d3d031c30b8b00ba56a0e6ea63ae35e2e97cf
prlhspt <prlhspt@naver.com>
2020-07-07 18:01:18 +0900

    MVC2

---
+++ JDBC/LHS~2.sql
@@ -0,0 +1,2 @@
+SELECT * FROM NOTICE;
+commit;
---
+++ JDBC/src/ex1/Program.java
@@ -0,0 +1,33 @@
+package ex1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "lhs", "1234");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next()) {
+			int id = rs.getInt("ID");
+			String title = rs.getString("Title");
+			String writerId = rs.getString("WRITERID");
+			
+			
+		rs.close();
+		st.close();
+		con.close();
+		}
+	}
+
+}
